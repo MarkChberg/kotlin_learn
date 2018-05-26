54ec69cb539108b8a73e263c0b6b0f52426562d6
mark <1766163080@qq.com>
2018-05-26 16:22:47 +0800

    learn kotlin

--- learn02/TestJoinner.java
+++ learn02/TestJoinner.java
@@ -9,7 +9,7 @@ public class TestJoinner {
         Set<String> set = new HashSet<>();
         set.add("ab");
         set.add("opp");
-        // 可以在java中直接使用kotlin的类，但是kotlin的部分特性会失效（比如此处的默认参数等）
+        // 可以在java中直接使用kotlin的类，但是kotlin的部分特性会失效（比如此处的默认参数等）  ...
         System.out.print(LearnJoinerKt.joinerToString(set, " ", "(", ")"));
     }
 }
